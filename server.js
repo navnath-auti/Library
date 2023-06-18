b19441e0e0689fe6d343768cd485f0f25735bd55
navnath-auti <navnathauti0510gmail.com>
2023-06-18 13:33:31 +0530

    nothing to commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose
     family: 4,
   })
   .then(() => {
-    console.log("mongodb database connection is established");
+    console.log("mongodb database connection is successful");
     app.listen(config.port, () => {
       console.log("App listening on port " + config.port);
     });
